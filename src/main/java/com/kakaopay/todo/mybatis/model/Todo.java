472984efd0ded1a9dcf5351b1e6b2b8625a753ef
164114 <164114@shinsegae.com>
2019-03-14 14:14:48 +0900

    remove getRegDts()

--- src/main/java/com/kakaopay/todo/mybatis/model/Todo.java
+++ src/main/java/com/kakaopay/todo/mybatis/model/Todo.java
@@ -1,7 +1,6 @@
 package com.kakaopay.todo.mybatis.model;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,14 +12,9 @@ import org.apache.ibatis.type.Alias;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Alias("todo")
 public class Todo {
-
     private Long id;
     private String contents;
     private String statusType;
     private LocalDateTime regDts;
     private LocalDateTime modDts;
-
-    public String getRegDts() {
-        return this.regDts.format(DateTimeFormatter.BASIC_ISO_DATE);
-    }
 }
